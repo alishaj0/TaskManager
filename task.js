9de988e3efb2b9e5f354138be335f2205aaf22e0
Alisha Bolin <alishabolin@KCCCs-MBP.lan>
2021-03-08 17:26:52 -0600

    Very unfinished assignment

---
+++ task.js
@@ -0,0 +1,34 @@
+// TASK MANAGER
+
+const menu = `TASK MANAGER
+
+What would you like to do:
+"TASKS" - Display All Tasks
+"NEW" - Add A New Task
+"REMOVE" - Remove A Task
+"CLOSE" - Close The Task Manager`;
+
+// const tasks = [`Charge MacBook', 'Master JavaScript`];
+let newTask;
+let removeTask;
+let userInput = prompt(menu);
+
+
+
+
+
+while (userInput.toUpperCase() !== "CLOSE") {
+    if (userInput.toUpperCase() === "TASKS"){
+        alert(`Charge MacBook
+        Master JaveSceript`);
+    } else (userInput.toUpperCase() === "CLOSE"){
+        alert(`Thank you for using Task Manager`);
+    }
+}
+
+
+//  I DO NOT UNDERSTAND!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//    ugh
+
+
